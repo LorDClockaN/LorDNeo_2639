7998227e04ca474816294f120016334f68950b75
Angelo G. Del Regno <kholk11@gmail.com>
2012-05-07 22:37:25 +0200

    staging: android, lowmemorykiller: convert to use oom_score_adj

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -1,16 +1,17 @@
 /* drivers/misc/lowmemorykiller.c
  *
  * The lowmemorykiller driver lets user-space specify a set of memory thresholds
- * where processes with a range of oom_adj values will get killed. Specify the
- * minimum oom_adj values in /sys/module/lowmemorykiller/parameters/adj and the
- * number of free pages in /sys/module/lowmemorykiller/parameters/minfree. Both
- * files take a comma separated list of numbers in ascending order.
+ * where processes with a range of oom_score_adj values will get killed. Specify
+ * the minimum oom_score_adj values in
+ * /sys/module/lowmemorykiller/parameters/adj and the number of free pages in
+ * /sys/module/lowmemorykiller/parameters/minfree. Both files take a comma
+ * separated list of numbers in ascending order.
  *
  * For example, write "0,8" to /sys/module/lowmemorykiller/parameters/adj and
  * "1024,4096" to /sys/module/lowmemorykiller/parameters/minfree to kill
- * processes with a oom_adj value of 8 or higher when the free memory drops
- * below 4096 pages and kill processes with a oom_adj value of 0 or higher
- * when the free memory drops below 1024 pages.
+ * processes with a oom_score_adj value of 8 or higher when the free memory
+ * drops below 4096 pages and kill processes with a oom_score_adj value of 0 or
+ * higher when the free memory drops below 1024 pages.
  *
  * The driver considers memory used for caches to be free, but if a large
  * percentage of the cached memory is locked this can be very inaccurate
@@ -75,7 +76,7 @@ static struct task_struct *lowmem_deathpending;
 static unsigned long lowmem_deathpending_timeout;
 static unsigned long lowmem_fork_boost_timeout;
 static uint32_t lowmem_fork_boost = 1;
-static int last_min_adj = OOM_ADJUST_MAX + 1;;
+static int last_min_adj = OOM_SCORE_ADJ_MAX + 1;;
 
 #define lowmem_print(level, x...)			\
 	do {						\
@@ -105,8 +106,8 @@ static void show_meminfo(void)
 /**
  * dump_tasks - dump current memory state of all system tasks
  *
- * State information includes task's pid, uid, tgid, vm size, rss, cpu, oom_adj
- * value, oom_score_adj value, and name.
+ * State information includes task's pid, uid, tgid, vm size, rss, cpu,
+ * oom_score_adj value, and name.
  *
  * Call with tasklist_lock read-locked.
  */
@@ -175,9 +176,9 @@ static int lowmem_shrink(struct shrinker *s, int nr_to_scan, gfp_t gfp_mask)
 	int rem = 0;
 	int tasksize;
 	int i;
-	int min_adj = OOM_ADJUST_MAX + 1;
+	int min_score_adj = OOM_SCORE_ADJ_MAX + 1;
 	int selected_tasksize = 0;
-	int selected_oom_adj;
+	int selected_oom_score_adj;
 	int array_size = ARRAY_SIZE(lowmem_adj);
 	int other_free = global_page_state(NR_FREE_PAGES);
 	int other_file = global_page_state(NR_FILE_PAGES) -
@@ -218,7 +219,7 @@ static int lowmem_shrink(struct shrinker *s, int nr_to_scan, gfp_t gfp_mask)
 	for (i = 0; i < array_size; i++) {
 		if (other_free < min_array[i] &&
 		    (other_file < min_array[i])) {
-			min_adj = adj_array[i];
+			min_score_adj = adj_array[i];
 			fork_boost = lowmem_fork_boost_minfree[i];
 			break;
 		}
@@ -226,22 +227,22 @@ static int lowmem_shrink(struct shrinker *s, int nr_to_scan, gfp_t gfp_mask)
 	if (nr_to_scan > 0)
 		lowmem_print(3, "lowmem_shrink %d, %x, ofree %d %d, ma %d\n",
 			     nr_to_scan, gfp_mask, other_free, other_file,
-			     min_adj);
+			     min_score_adj);
 	rem = global_page_state(NR_ACTIVE_ANON) +
 		global_page_state(NR_ACTIVE_FILE) +
 		global_page_state(NR_INACTIVE_ANON) +
 		global_page_state(NR_INACTIVE_FILE);
-	if (nr_to_scan <= 0 || min_adj == OOM_ADJUST_MAX + 1) {
+	if (nr_to_scan <= 0 || min_score_adj == OOM_SCORE_ADJ_MAX + 1) {
 		lowmem_print(5, "lowmem_shrink %d, %x, return %d\n",
 			     nr_to_scan, gfp_mask, rem);
 		return rem;
 	}
-	selected_oom_adj = min_adj;
+	selected_oom_score_adj = min_score_adj;
 
 	rcu_read_lock();
 	for_each_process(tsk) {
 		struct task_struct *p;
-		int oom_adj;
+		int oom_score_adj;
 
 		if (tsk->flags & PF_KTHREAD)
 			continue;
@@ -250,9 +251,9 @@ static int lowmem_shrink(struct shrinker *s, int nr_to_scan, gfp_t gfp_mask)
 		if (!p)
 			continue;
 
-		oom_adj = p->signal->oom_adj;
+		oom_score_adj = p->signal->oom_score_adj;
 
-		if (oom_adj < min_adj) {
+		if (oom_score_adj < min_score_adj) {
 			task_unlock(p);
 			continue;
 		}
@@ -261,36 +262,36 @@ static int lowmem_shrink(struct shrinker *s, int nr_to_scan, gfp_t gfp_mask)
 		if (tasksize <= 0)
 			continue;
 		if (selected) {
-			if (oom_adj < selected_oom_adj)
+			if (oom_score_adj < selected_oom_score_adj)
 				continue;
-			if (oom_adj == selected_oom_adj &&
+			if (oom_score_adj == selected_oom_score_adj &&
 			    tasksize <= selected_tasksize)
 				continue;
 		}
 		selected = p;
 		selected_tasksize = tasksize;
-		selected_oom_adj = oom_adj;
+		selected_oom_score_adj = oom_score_adj;
 		lowmem_print(2, "select %d (%s), adj %d, size %d, to kill\n",
-			     p->pid, p->comm, oom_adj, tasksize);
+			     p->pid, p->comm, oom_score_adj, tasksize);
 	}
 
 	if (selected) {
-		if (last_min_adj > selected_oom_adj &&
-			(selected_oom_adj == 12 || selected_oom_adj == 9 || selected_oom_adj == 7)) {
-			last_min_adj = selected_oom_adj;
-			lowmem_print(1, "lowmem_shrink: monitor memory status at selected_oom_adj=%d\n", selected_oom_adj);
+		if (last_min_adj > selected_oom_score_adj &&
+			(selected_oom_score_adj == 12 || selected_oom_score_adj == 9 || selected_oom_score_adj == 7)) {
+			last_min_adj = selected_oom_score_adj;
+			lowmem_print(1, "lowmem_shrink: monitor memory status at selected_oom_score_adj=%d\n", selected_oom_score_adj);
 			show_meminfo();
 			dump_tasks();
 		}
 
-		lowmem_print(1, "[%s] send sigkill to %d (%s), adj %d, size %dK, min_adj=%d,"
+		lowmem_print(1, "[%s] send sigkill to %d (%s), adj %d, size %dK, min_score_adj=%d,"
 			" free=%dK, file=%dK, fork_boost=%d\n",
 			     current->comm, selected->pid, selected->comm,
-			     selected_oom_adj, selected_tasksize << 2, min_adj,
+			     selected_oom_score_adj, selected_tasksize << 2, min_score_adj,
 			     other_free << 2, other_file << 2, fork_boost << 2);
 		lowmem_deathpending = selected;
 		lowmem_deathpending_timeout = jiffies + HZ;
-		if (selected_oom_adj < 7)
+		if (selected_oom_score_adj < 7)
 		{
 			show_meminfo();
 			dump_tasks();
